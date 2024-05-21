26be0b750115a9893c3172ac1b2799349b54c09b
Thomas Tsai <tlinux.tsai@gmail.com>
2024-05-21 17:18:19 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'NCHC KASM WORKSPACE REGISTRY',
+    description: 'The nchc store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://nchcdev.github.io/workspace_registry/',
+    contactUrl: 'https://github.com/nchcdev/workspace_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
